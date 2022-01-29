73e1ec8e491bc0d10060a6f2ee0f1eb15a7be02d
manaal khan <mk90@rice.edu>
2022-01-29 13:45:24 -0600

    works

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/forecast', methods=['POST'])
 def predict():
-    json_ = request.json
+    # json_ = request.json
     # query_df = pd.DataFrame(json_)
     # query = pd.get_dummies(query_df)
 
@@ -21,4 +21,4 @@ def predict():
     
     # classifier = joblib.load('datathon.pkl')
     # prediction = classifier.predict(query)
-    return jsonify({'prediction': forecast})
+    return jsonify({'prediction': list(forecast)})
